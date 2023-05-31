f84cbc328ba4d16d431e61a05fd284d9878a88c5
Monster <1598435602@qq.com>
2023-05-31 13:13:03 +0800

    update

--- Js/BiliBili/BiliBili_quality.js
+++ Js/BiliBili/BiliBili_quality.js
@@ -1,11 +1,10 @@
 #!name = Bilibili去广告
-#!desc = 过滤Bilibili广告、移除青少年模式弹窗和交互式弹幕、移除无用功能。
 
 
 [Script]
 # 解锁4K画质及1080P高码率 //app.bilibili.com
-http-response ^https:\/\/app\.bilibili\.com\/x\/v2\/account\/myinfo\? script-path = https://raw.githubusercontent.com/Fantuan-cell/Loon/main/Js/Bilibili_vip.js, requires-body = true, tag = 画质解锁
+http-response ^https:\/\/app\.bilibili\.com\/x\/v2\/account\/myinfo\? script-path = https://raw.githubusercontent.com/Fantuan-cell/Loon/main/Js/Bilibili_vip.js, requires-body = true,,timeout=30,tag=哔哩哔哩高画质, enabled=true
 
 
 [MITM]
-hostname = app.bilibili.com,api.bilibili.com
+hostname = app.bilibili.com
