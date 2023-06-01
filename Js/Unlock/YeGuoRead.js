71d0e19dbd830b6f68ad034b1cec536f082d64b4
Monster <1598435602@qq.com>
2023-06-01 18:05:09 +0800

    update

--- Js/Unlock/YeGuoRead.js
+++ Js/Unlock/YeGuoRead.js
@@ -3,17 +3,12 @@
 http-response ^https:\/\/yeguo\.236api\.com\/v1\/my\/info$  requires-body=true,script-path=https://raw.githubusercontent.com/Fantuan-cell/Loon/main/Js/Unlock/YeGuoRead.js
 
 */
-url = $response.url;
-host = ""
+
 var body = $response.body;;
 
 var hack = /vip_end_date":\d+/g;
 
 body = body.replace(hack,'vip_end_date":99999999');
-if(url =="https:\/\/yeguo.236api.com\/v1\/my\/info"){
-    var name = /nickname":".*?",/g;
-    body = body.replace(name,`nickname":"Mny",`);
-}
 $done({
     body:JSON.stringify(body)
 });
